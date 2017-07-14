f4053776154828a9c55b8b30032079ba85d8beb9
mtnorton <mnorton@chesapeakeconservancy.org>
2017-07-14 16:28:49 -0400

    BFAST - first try

--- BFAST.R
+++ BFAST.R
@@ -1,5 +1,25 @@
 #BFAST package updating
 # Started 7.13.2017 by Mike Norton
 
-library(bfast)
+library(utils)
+library(stringr)
 
+setwd("F:\\Landsat7\\")
+
+
+filelist <- list.files("F:\\Landsat7\\")
+
+
+
+for (i in 1:length(filelist))
+{
+  tarlist <- untar(paste0("F:\\Landsat7\\",filelist[i]), list=TRUE)
+  ndvi_img <- grep("ndvi.tif",tarlist)
+  
+  if (!is.na(tarlist[ndvi_img]))
+  {
+    date <- substr(tarlist[ndvi_img],18,25)
+  }
+  
+  writeLines(date)
+}
